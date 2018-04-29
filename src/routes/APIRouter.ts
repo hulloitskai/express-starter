44dab9dc23358d7b51ccdbfe3c0e51b2dadd3d5f
steven-xie <dev@stevenxie.me>
2018-04-29 08:51:54 -0400

    Added Prettier config, re-styled src files

--- src/routes/APIRouter.ts
+++ src/routes/APIRouter.ts
@@ -1,15 +1,11 @@
-import CustomRouter from "../common/CustomRouter";
-import puppyRouter from "./PuppyRouter";
+import CustomRouter from '../common/CustomRouter';
+import puppyRouter from './PuppyRouter';
 
 export class APIRouter extends CustomRouter {
-  constructor() {
-    super();
-  }
-
-  registerRoutes() {
-    this.router.get("/", (_, res) => res.json("The API is working!"));
-    this.router.use("/puppies", puppyRouter);
-  }
+	registerRoutes() {
+		this.router.get('/', (_, res) => res.json('The API is working!'));
+		this.router.use('/puppies', puppyRouter);
+	}
 }
 
 export default new APIRouter().export();
